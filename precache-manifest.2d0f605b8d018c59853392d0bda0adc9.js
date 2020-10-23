f2202258a720835856e8eb9a2a2a2edef639dcfe
BrunoLagoa <miroldols@gmail.com>
2020-10-23 13:53:45 -0300

    Updates

---
+++ precache-manifest.2d0f605b8d018c59853392d0bda0adc9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e3eea79e58798da4787a90f514c854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53979432a39b3a8165e",
+    "url": "/static/js/2.fd8656a8.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.fd8656a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82cb3cbf918ead6e9fc",
+    "url": "/static/js/main.f46b42ff.chunk.js"
+  },
+  {
+    "revision": "668318a814945cf610f0",
+    "url": "/static/js/runtime-main.5b460e13.js"
+  },
+  {
+    "revision": "8f33398725d48caa29849528e441c7cd",
+    "url": "/static/media/Accessories.8f333987.jpeg"
+  },
+  {
+    "revision": "61c20079fa66da92f725788f20200a44",
+    "url": "/static/media/Model3.61c20079.jpeg"
+  },
+  {
+    "revision": "8439103915e0fd907620f5e457efb07d",
+    "url": "/static/media/ModelS.84391039.jpeg"
+  },
+  {
+    "revision": "c2be2af76f3d608c13814cb6d7afc0c3",
+    "url": "/static/media/ModelX.c2be2af7.jpeg"
+  },
+  {
+    "revision": "28c6a778248ca2a5ad864b64c61fafc5",
+    "url": "/static/media/ModelY.28c6a778.jpeg"
+  },
+  {
+    "revision": "a1e04a413333039f8590cefadb42bdc1",
+    "url": "/static/media/SolarPanels.a1e04a41.jpeg"
+  },
+  {
+    "revision": "fe70c48e08276958fd091533705d1529",
+    "url": "/static/media/SolarRoof.fe70c48e.jpeg"
+  },
+  {
+    "revision": "2555ace976af4d7fd91ca4ddfa5a3c47",
+    "url": "/static/media/tesla.2555ace9.svg"
+  }
+]);
